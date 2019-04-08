9fdfa747f9a04709c6179bd8deb265175b52c21e
andr4229 <andr4229@hotmail.com>
2019-04-08 11:15:25 +0200

    checks flyttet til bll

--- AttendanceAutomation/src/attendanceautomation/bll/PassThroughLayer.java
+++ AttendanceAutomation/src/attendanceautomation/bll/PassThroughLayer.java
@@ -11,6 +11,7 @@ import attendanceautomation.be.Teacher;
 import attendanceautomation.dal.GetData;
 import attendanceautomation.dal.IGetData;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -51,7 +52,27 @@ public class PassThroughLayer implements IBLL
      * @param student 
      */
     public void registerAttendance(Attendance attendance, Student student){
-        data.registerAttendance(attendance, student);
+        boolean alreadyExist = false;
+        for (Attendance attendance1 : student.getAttendanceDates1())
+        {
+            if(attendance1.getDateAsDate().equals(LocalDate.now())){
+                if(!attendance1.getAbsense().equals(attendance.getAbsense())){
+                    data.editAttendance(new Attendance(student.getId(), LocalDate.now(), attendance.getAbsense()), student);
+                    //attendance.set eller attendance.edit
+                    alreadyExist = true;
+                }
+                else if(attendance.getAbsense().equals(attendance.getAbsense())){
+                    //do nothing
+                    alreadyExist = true;
+                }
+            }
+        }
+        if (!alreadyExist){
+            //model.registerAttendance
+            Attendance a1 = new Attendance(student.getId(), LocalDate.now(), attendance.getAbsense());
+            data.registerAttendance(a1, student);
+            student.addAttendanceDate(LocalDate.now(), attendance.getAbsense());
+        }
     }
     
     /**
@@ -61,5 +82,17 @@ public class PassThroughLayer implements IBLL
      */
     public void editAttendance(Attendance attendance, Student student){
         data.editAttendance(attendance, student);
+        for (Attendance attendance1 : student.getAttendanceDates1()) {
+            if (!attendance1.getAbsense().equals(attendance.getAbsense())) {
+                
+                Thread t = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        data.editAttendance(attendance, student);
+                    }
+                });
+                t.start();
+            }
+        }
     }
 }
